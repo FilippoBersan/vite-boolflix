927715562b544949cb67d07a74d4205199e2772b
FilippoBersan <filippobersan99@gmail.com>
2023-12-15 10:09:09 +0100

    feat: milestone 0 - store - config

--- src/store.js
+++ src/store.js
@@ -3,6 +3,8 @@ import { reactive } from 'vue';
 export const store = reactive({
   film: [],
 
-  apiUrl:
-    'https://api.themoviedb.org/3/search/movie?api_key=fefaeabe53117f1cbd0449d990e58215&query=futuro',
+  config: {
+    apiFilms: 'https://api.themoviedb.org/3/search/movie',
+    apiKey: 'fefaeabe53117f1cbd0449d990e58215',
+  },
 });
