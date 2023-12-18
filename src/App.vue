9b759541cdba54747aca3111d33e0ad7f5c23f90
FilippoBersan <filippobersan99@gmail.com>
2023-12-18 14:10:33 +0100

    feat: milestone 1 completa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import { store } from './store';
+
 import axios from 'axios';
 
 export default {
   name: 'BoolFLix',
+  components: {},
   data() {
     return {
       store,
@@ -27,11 +29,26 @@ export default {
 </script>
 
 <template>
-  <form>
-    <label @submit.prevent="search">Search</label>
+  <form @submit.prevent="search">
+    <label for="search">Search</label>
     <input type="text" id="search" v-model="store.searchKey" />
     <button>Search</button>
   </form>
+
+  <ul>
+    <li>
+      <div v-for="films in store.film">
+        <h4>{{ films.title }}</h4>
+        <h5>{{ films.original_title }}</h5>
+        <p>{{ films.original_language }}</p>
+        <p>{{ films.vote_average }}</p>
+      </div>
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+li {
+  list-style-type: none;
+}
+</style>
